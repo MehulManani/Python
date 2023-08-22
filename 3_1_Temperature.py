aef9b50468c5f977b580b52328401ec390c7b215
Mehul_Manani <mehul.gecr@gmail.com>
2023-08-22 12:14:09 +0530

    Add files via upload

---
+++ 3_1_Temperature.py
@@ -0,0 +1,14 @@
+"""
+Practical : 3.1
+Aim: Write a program to convert temperature from Celsius to Fahrenheit.
+     Equation to convert Celsius to Fahrenheit is : 
+     F = (9/5) * C + 32 
+Date:15/09/2022
+Enrollment No: 216450307001
+Name: Mehul Manani
+"""
+celsius = float(input("Enter the temperature value in Celsius: \n"))
+
+Fahreinheit = (9/5) * celsius + 32
+
+print (f"The Fahreinheit value of {celsius} Celsius is : {Fahreinheit}")
